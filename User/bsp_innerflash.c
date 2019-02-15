9fd73c0723dc6544eb2d075f471df2d0b6a472f0
mx92054 <chenm@idsse.ac.cn>
2019-02-15 21:14:31 +0800

    commit second

--- User/bsp_innerflash.c
+++ User/bsp_innerflash.c
@@ -115,8 +115,6 @@ void Flash_Write32BitDatas(uint32_t address, uint16_t length, int32_t *data_32)
     }
   }
   FLASH_Lock(); //读FLASH不需要FLASH处于解锁状态。
-
-  add line
 }
 
 /****************************************************************************
@@ -251,3 +249,5 @@ void Flash_Read8BitDatas(uint32_t address, uint16_t length, int8_t *data_8)
     address++;
   }
 }
+
+//---------------------------end of file----------------------------------------
