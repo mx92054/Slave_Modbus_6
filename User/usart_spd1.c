30322882d7e618753e54f8ccc6071e8ca80209d5
mx92054 <chenm@idsse.ac.cn>
2019-02-16 14:26:47 +0800

    commit

--- User/usart_spd1.c
+++ User/usart_spd1.c
@@ -130,6 +130,7 @@ void SPD1_Init(void)
 //-------------------------------------------------------------------------------
 void SPD1_TxCmd(void)
 {
+    u8 bFirst = 1 ;
     u16 uCRC;
 
     if (SPD1_bRecv == 1) //如果当前未完成接收，则通信错误计数器递增
@@ -138,7 +139,7 @@ void SPD1_TxCmd(void)
     SPD1_curptr = 0;
     SPD1_bRecv = 1;
 
-    if (bChanged)
+    if (bChanged || bFirst)
     {
         SPD1_frame[0] = wReg[108];                 //station number
         SPD1_frame[2] = (wReg[109] & 0xff00) >> 8; //start address high
@@ -149,6 +150,7 @@ void SPD1_TxCmd(void)
         SPD1_frame[6] = uCRC & 0x00FF;        //CRC low
         SPD1_frame[7] = (uCRC & 0xFF00) >> 8; //CRC high
         bChanged++;
+        bFirst = 0 ;
     }
 
     Usart_SendBytes(USART_SPD1, SPD1_frame, 8);
