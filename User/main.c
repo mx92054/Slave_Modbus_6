c0a0c2e2ca7718a6a8e9f64b77d2a2547afc5c00
mx92054 <chenm@idsse.ac.cn>
2019-02-16 18:47:46 +0800

    commit

--- User/main.c
+++ User/main.c
@@ -24,36 +24,29 @@
 extern short wReg[];
 extern u8 bChanged;
 
-extern uint8_t cpt_frame[];
-
-/**
-  * @brief  主函数
-  * @param  无
-  * @retval 无
-  */
 int main(void)
 {
 
-	SysTick_Init();
-	GPIO_Config();
-	Flash_Read16BitDatas(FLASH_USER_START_ADDR, 200, wReg);
+	SysTick_Init();											//tick定时器初始
+	GPIO_Config();											//GPIO初始化
+	Flash_Read16BitDatas(FLASH_USER_START_ADDR, 200, wReg); //通信寄存器初始化
 
-	Modbus_init();
-	ALT_Init();
-	SPD1_Init();
-	SPD2_Init();
-	SPD3_Init();
+	Modbus_init(); //上位机通信初始化
+	ALT_Init();	//高度计通信初始化
+	SPD1_Init();   //1#编码器通信初始化
+	SPD2_Init();   //2#编码器通信初始化
+	SPD3_Init();   //3#编码器通信初始化
 
 	SetTimer(0, 500);
 	SetTimer(1, 1000);
 	SetTimer(2, 200);
 	SetTimer(3, 100);
 
-	IWDG_Configuration();
+	IWDG_Configuration(); //看门狗初始
 
 	while (1)
 	{
-		Modbus_task();
+		Modbus_task(); //通信出来进程
 		ALT_Task();
 		SPD1_Task();
 		SPD2_Task();
@@ -61,28 +54,28 @@ int main(void)
 
 		if (GetTimer(0))
 		{
-			IWDG_Feed();
+			IWDG_Feed(); //看门狗复位
 			LOGGLE_LED2;
-			if (bSaved)
-			{
-				Flash_Write16BitDatas(FLASH_USER_START_ADDR, 200, wReg);
-				bSaved = 0;
-			}
-
 			if (bChanged > 10)
 				bChanged = 0;
 		}
 
+		if (GetTimer(1) && bSaved)
+		{
+			Flash_Write16BitDatas(FLASH_USER_START_ADDR, 200, wReg); //保存修改过的寄存器
+			bSaved = 0;
+		}
+
 		if (GetTimer(2))
 		{
-			ALT_TxCmd();
+			ALT_TxCmd(); //向深度计发读取指令
 		}
 
 		if (GetTimer(3))
 		{
-			SPD1_TxCmd();
-			SPD2_TxCmd();
-			SPD3_TxCmd();
+			SPD1_TxCmd(); //向1#编码器发读取指令
+			SPD2_TxCmd(); //向2#编码器发读取指令
+			SPD3_TxCmd(); //向3#编码器发读取指令
 		}
 	}
 }
